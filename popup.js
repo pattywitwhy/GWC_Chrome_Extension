e73a49c5e86ebd4032232771487b63efc36c3f5d
pattywitwhy <pattyxhwang@gmail.com>
2019-06-30 18:43:28 -0500

    added custom links and color

--- popup.js
+++ popup.js
@@ -1,33 +1,13 @@
-function actualize() {
-  chrome.tabs.create({url: "https://anyonecanlearntocode.com/chicago-coding-bootcamp"});
+function namely() {
+  chrome.tabs.create({url: "https://girlswhocode.namely.com/"});
 }
-function killnil() {
-  chrome.tabs.create({url: "https://github.com/killnil"});
+function GWCportal() {
+  chrome.tabs.create({url: "https://gwcportal.force.com/partnerresources/s/"});
 }
-function github() {
-  chrome.tabs.create({url: "https://github.com/"});
-}
-function google() {
-  chrome.tabs.create({url: "https://docs.google.com/"});
-}
-function rubydocs() {
-  chrome.tabs.create({url: "https://ruby-doc.org/"});
-}
-function w3schools() {
-  chrome.tabs.create({url: "https://www.w3schools.com/"});
-}
-function htmlcolorcodes() {
-  chrome.tabs.create({url: "https://htmlcolorcodes.com/"});
-}
-function rubygems() {
-  chrome.tabs.create({url: "https://rubygems.org/"});
+function hq() {
+  chrome.tabs.create({url: "https://hq.girlswhocode.com/gwc#collection/sip-u1-fundamentals"});
 }
 
-document.querySelector('.actualize').addEventListener('click', actualize);
-document.querySelector('.killnil').addEventListener('click', killnil);
-document.querySelector('.github').addEventListener('click', github);
-document.querySelector('.google').addEventListener('click', google);
-document.querySelector('.rubydocs').addEventListener('click', rubydocs);
-document.querySelector('.w3schools').addEventListener('click', w3schools);
-document.querySelector('.htmlcolorcodes').addEventListener('click', htmlcolorcodes);
-document.querySelector('.rubygems').addEventListener('click', rubygems);
+document.querySelector('.namely').addEventListener('click', namely);
+document.querySelector('.GWCportal').addEventListener('click', GWCportal);
+document.querySelector('.hq').addEventListener('click', hq);
